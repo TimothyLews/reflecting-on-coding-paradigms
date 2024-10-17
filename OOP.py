d66b4d63c73f4413ae889353a27817b39c7b5c6a
TimothyLewis <timlew2003@gmail.com>
2024-10-17 14:54:07 -0700

    add Podracer class with attributes and repair method

---
+++ OOP.py
@@ -0,0 +1,9 @@
+# Podracer Class
+class Podracer:
+    def __init__(self, max_speed, condition, price):
+        self.max_speed = max_speed
+        self.condition = condition
+        self.price = price
+
+    def repair(self):
+        self.condition = "repaired"
