accfc56ddc498816de3bd590c60f870c805b395e
TimothyLewis <timlew2003@gmail.com>
2024-10-17 14:57:33 -0700

    added AnakinsPod subclass with boost method and added SebulbasPod subclass with flame_jet method

--- OOP.py
+++ OOP.py
@@ -7,3 +7,26 @@ class Podracer:
 
     def repair(self):
         self.condition = "repaired"
+
+# AnakinsPod Subclass
+class AnakinsPod(Podracer):
+    def boost(self):
+        self.max_speed *= 2
+
+# SebulbasPod Subclass
+class SebulbasPod(Podracer):
+    def flame_jet(self, other_pod):
+        other_pod.condition = "trashed"
+
+pod = Podracer(500, "perfect", 10000)
+print(pod.condition)  # Output: perfect
+pod.repair()
+print(pod.condition)  # Output: repaired
+
+anakins_pod = AnakinsPod(200, "perfect", 15000)
+anakins_pod.boost()
+print(anakins_pod.max_speed)  # Output: 400
+
+sebulbas_pod = SebulbasPod(300, "trashed", 12000)
+sebulbas_pod.flame_jet(anakins_pod)
+print(anakins_pod.condition)  # Output: trashed
